2e0655daaa69dcb8dcfa4c3582ebf66bbbb47e04
fuquanlin <fuquanlin@jk.cn>
2016-11-18 18:34:56 +0800

    add bubblo

--- src/main/java/cn/fql/coderush/algorithm/BubbleSort.java
+++ src/main/java/cn/fql/coderush/algorithm/BubbleSort.java
@@ -1,10 +1,28 @@
 package cn.fql.coderush.algorithm;
 
+import java.util.Arrays;
+
 /**
  * Created by fuquanlin on 2016/11/18.
  */
 public class BubbleSort {
+
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+
     public static void main(String[] args) {
-        System.out.println("sdfsdf");
+        int[] array = {5, 4, 2, 1, 3, 9, 7, 8, 0};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j <array.length; j++) {
+                if (array[i] > array[j]) {
+                    swap(array, i, j);
+                }
+            }
+        }
+        Arrays.stream(array).forEach(a-> System.out.printf(a+""));
     }
 }
--- src/main/java/cn/fql/coderush/algorithm/QuickSort.java
+++ src/main/java/cn/fql/coderush/algorithm/QuickSort.java
@@ -4,4 +4,20 @@ package cn.fql.coderush.algorithm;
  * Created by fuquanlin on 2016/11/18.
  */
 public class QuickSort {
+
+    public static void quick(){
+
+    }
+
+    public static void sort(int array, int low, int high){
+
+    }
+k
+    public static void getMiddle(int array, int low, int high){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
