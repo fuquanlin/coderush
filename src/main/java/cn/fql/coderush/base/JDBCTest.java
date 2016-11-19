ffb6f8f019debf8541df119d38eaa10cc8e3682e
fuquanlin <fuquanlin@gmail.com>
2016-11-19 23:30:04 +0800

    add singleton and JDBC

--- src/main/java/cn/fql/coderush/base/JDBCTest.java
+++ src/main/java/cn/fql/coderush/base/JDBCTest.java
@@ -1,7 +1,18 @@
 package cn.fql.coderush.base;
 
+import java.sql.*;
+
 /**
  * Created by fuquanlin on 2016/11/18.
  */
 public class JDBCTest {
+    public static void main(String[] args) throws Exception {
+        Class.forName("")       ;
+        Connection connection = DriverManager.getConnection("");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("");
+        while(resultSet.next() ){
+            System.out.println(resultSet.getString(1));
+        }
+    }
 }
---
+++ src/main/java/cn/fql/coderush/pattern/Singleton.java
@@ -0,0 +1,19 @@
+package cn.fql.coderush.pattern;
+
+/**
+ * Created by fuquanlin on 19/11/2016.
+ */
+public class Singleton {
+    private static Singleton singleton;
+
+    private Singleton(){
+
+    }
+
+    public synchronized static Singleton getInstance(){
+         if(singleton==null){
+             singleton = new Singleton();
+         }
+         return singleton;
+    }
+}
